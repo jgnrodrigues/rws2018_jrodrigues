24b8b5059366fcfeec4f8e7350688d82d4c39c18
Gabriel <gabriel.rodrigues@ua.pt>
2018-03-01 16:43:44 +0000

    post position

--- player_jrodrigues/src/player_jrodrigues_node.cpp
+++ player_jrodrigues/src/player_jrodrigues_node.cpp
@@ -1,13 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <boost/shared_ptr.hpp>
+#include <ros/ros.h>
 
 #include "teams.h"
+#include <rws2018_libs/team.h>
+#include <tf/transform_broadcaster.h>
+
+using namespace std;
+
+namespace rws_jrodrigues
+{
+// class Team
+// {
+//   public:
+//     /**
+//        * @brief Constructor
+//        * @param team_name the team name
+//        */
+//     Team(string team_name)
+//     {
+//         name = team_name;
+//     }
+
+//     /**
+//        * @brief Prints the name of the team and the names of all its players
+//        */
+//     void printTeamInfo(void)
+//     {
+//         //TODO
+//     }
+
+//     /**
+//        * @brief Checks if a player belongs to the team
+//        * @param player_name the name of the player to check
+//        * @return true or false, yes or no
+//        */
+//     bool playerBelongsToTeam(string player_name)
+//     {
+//         //write code here ...
+//     }
+
+//     /**
+//        * @brief The team name
+//        */
+//     string name;
+
+//     /**
+//        * @brief A list of the team's player names
+//        */
+//     vector<string> players;
+// };
 
 class Player
 {
   public:
-    Player(std::string name)
+    Player(string name)
     {
         this->name = name;
+        this->x = 5.7;
+        this->y = 4.6;
+        this->theta = 0.0;
     }
 
     //Set team name, if given a correct team name (accessor)
@@ -26,12 +79,12 @@ class Player
             return setTeamName("blue");
             break;
         default:
-            std::cout << "wrong team index given. Cannot set team" << std::endl;
+            cout << "wrong team index given. Cannot set team" << endl;
             break;
         }
     }
 
-    int setTeamName(std::string team)
+    int setTeamName(string team)
     {
         if (team == "red" || team == "green" || team == "blue")
         {
@@ -40,42 +93,88 @@ class Player
         }
         else
         {
-            std::cout << "cannot set team name to " << team << std::endl;
+            cout << "cannot set team name to " << team << endl;
             return 0;
         }
     }
 
-    //Gets team name (accessor)
-    std::string getTeam(void) { return team; }
+    void move(void)
+    {
+        static tf::Transform transform;
+        transform.setOrigin(tf::Vector3(this->x, this->y, 0.0));
+        tf::Quaternion q;
+        q.setRPY(0, 0, this->theta);
+        transform.setRotation(q);
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", this->name));
+    }
 
-    std::string name;
+    //Gets team name (accessor)
+    string getTeam(void) { return team; }
+
+    string name;
+    boost::shared_ptr<Team> red_team;
+    boost::shared_ptr<Team> blue_team;
+    boost::shared_ptr<Team> green_team;
+    tf::TransformBroadcaster br;
+
+    double x;
+    double y;
+    double theta;
 
   private:
-    std::string team;
+    string team;
 };
 
 class MyPlayer : public Player
 {
   public:
-    MyPlayer(std::string name, std::string team) : Player(name)
+    MyPlayer(string name, string team) : Player(name)
     {
         setTeamName(team);
+
+        red_team = boost::shared_ptr<Team>(new Team("red"));
+        green_team = boost::shared_ptr<Team>(new Team("green"));
+        blue_team = boost::shared_ptr<Team>(new Team("blue"));
+
+        cout << "player.name is " << this->name << endl;
+        cout << "team is " << this->getTeam() << endl;
     }
 
-    MyPlayer(std::string name, int team) : Player(name)
+    MyPlayer(string name, int team) : Player(name)
     {
         setTeamName(team);
+
+        red_team = boost::shared_ptr<Team>(new Team("red"));
+        green_team = boost::shared_ptr<Team>(new Team("green"));
+        blue_team = boost::shared_ptr<Team>(new Team("blue"));
+
+        cout << "player.name is " << this->name << endl;
+        cout << "team is " << this->getTeam() << endl;
     }
 };
+}
 
-int main()
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "jrodrigues");
+    ros::NodeHandle n;
+    ros::Rate loop_rate(10);
 
-    std::string player_name = "jrodrigues";
+    string player_name = "jrodrigues";
     //Creating an instance of class Player
-    Player player(player_name);
-    player.setTeamName(RED);
+    string team;
+    n.getParam("team", team);
+    rws_jrodrigues::MyPlayer player(player_name, BLUE);
 
-    std::cout << "player.name is " << player.name << std::endl;
-    std::cout << "team is " << player.getTeam() << std::endl;
+    // cout << "Read team: " << team << endl;
+    // ros::spin();
+
+    while (ros::ok())
+    {
+        player.move();
+
+        ros::spinOnce();
+
+        loop_rate.sleep();
+    }
 }
--- player_jrodrigues/src/teams.h
+++ player_jrodrigues/src/teams.h
@@ -1,3 +1,8 @@
+#ifndef _TEAMS_
+#define _TEAMS_
+
 const int RED = 0;
 const int GREEN = 1;
 const int BLUE = 2;
+
+#endif
